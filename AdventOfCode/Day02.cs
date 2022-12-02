b9187f725514d2888c7ccba49cc4c39422760002
jlintvedt <jlintvedt@gmail.com>
2022-12-02 14:37:17 +0100

    Solve Day02 Puzzle1

--- AdventOfCode/Day02.cs
+++ AdventOfCode/Day02.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AdventOfCode
 {
@@ -7,10 +8,94 @@ namespace AdventOfCode
     /// </summary>
     public class Day02
     {
+        public class RockPaperScissor
+        {
+            private List<Round> rounds= new List<Round>();
+
+            public RockPaperScissor(string input)
+            {
+                foreach (var row in input.Split(Environment.NewLine))
+                {
+                    rounds.Add(new Round(row));
+                }
+            }
+
+            public int CalculateScoreFollowingGuide()
+            {
+                var sum = 0;
+                foreach (var round in rounds)
+                {
+                    sum += round.Score;
+                }
+                return sum;
+            }
+
+            private class Round
+            {
+                private Shape opponentShape;
+                private Shape playerShape;
+                public int Score;
+
+                public Round(string input)
+                {
+                    opponentShape = GetShape(input[0]);
+                    playerShape = GetShape(input[2]);
+
+                    Score = RoundScore() + (int)playerShape;
+                }
+
+                private Shape GetShape(char symbol)
+                {
+                    switch (symbol)
+                    {
+                        case 'A':
+                        case 'X':
+                            return Shape.Rock;
+                        case 'B':
+                        case 'Y':
+                            return Shape.Paper;
+                        case 'C':
+                        case 'Z':
+                            return Shape.Scissor;
+                        default:
+                            throw new ArgumentException();
+                    }
+                }
+
+                private int RoundScore()
+                {
+                    // Win
+                    if ((opponentShape == Shape.Rock && playerShape == Shape.Paper) ||
+                        (opponentShape == Shape.Paper && playerShape == Shape.Scissor) ||
+                        (opponentShape == Shape.Scissor && playerShape == Shape.Rock))
+                    {
+                        return 6;
+                    }
+                    // Loss
+                    if ((opponentShape == Shape.Rock && playerShape == Shape.Scissor) ||
+                        (opponentShape == Shape.Paper && playerShape == Shape.Rock) ||
+                        (opponentShape == Shape.Scissor && playerShape == Shape.Paper))
+                    {
+                        return 0;
+                    }
+                    // Draw
+                    return 3;
+                }
+            }
+        }
+
+        public enum Shape
+        {
+            Rock = 1,
+            Paper = 2,
+            Scissor = 3,
+        }
+
         // == == == == == Puzzle 1 == == == == ==
         public static string Puzzle1(string input)
         {
-            return "Puzzle1";
+            var rps = new RockPaperScissor(input);
+            return rps.CalculateScoreFollowingGuide().ToString();
         }
 
         // == == == == == Puzzle 2 == == == == ==
--- AdventOfCodeTests/Day02Tests.cs
+++ AdventOfCodeTests/Day02Tests.cs
@@ -3,7 +3,6 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AdventOfCodeTests
 {
-    [Ignore]
     [TestClass]
     public class Day02Tests
     {
@@ -16,7 +15,7 @@ namespace AdventOfCodeTests
         {
             string day = "02";
             input_puzzle = Resources.Input.ResourceManager.GetObject($"D{day}_Puzzle").ToString();
-            input_example1 = string.Format("example{0}1", Environment.NewLine);
+            input_example1 = string.Format("A Y{0}B X{0}C Z", Environment.NewLine);
             input_example2 = string.Format("example{0}2", Environment.NewLine);
         }
 
@@ -33,7 +32,7 @@ namespace AdventOfCodeTests
             var result = AdventOfCode.Day02.Puzzle1(input_example1);
 
             // Assert
-            Assert.AreEqual($"Puzzle1", result);
+            Assert.AreEqual($"15", result);
         }
 
         [TestMethod]
@@ -43,7 +42,7 @@ namespace AdventOfCodeTests
             var result = AdventOfCode.Day02.Puzzle1(input_puzzle);
 
             // Assert
-            Assert.AreEqual($"Puzzle1", result);
+            Assert.AreEqual($"13675", result);
         }
 
         [TestMethod]
