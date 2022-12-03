3fce673af53f20f0818cc59f6aab358befb9bdf3
jlintvedt <jlintvedt@gmail.com>
2022-12-03 09:24:59 +0100

    Solve Day03 Puzzle1

--- AdventOfCode/Day03.cs
+++ AdventOfCode/Day03.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AdventOfCode
 {
@@ -7,10 +8,79 @@ namespace AdventOfCode
     /// </summary>
     public class Day03
     {
+        public class RucksackReorganization
+        {
+            private List<Rucksack> rucksacks= new List<Rucksack>();
+
+            public RucksackReorganization(string input)
+            {
+                foreach (var items in input.Split(Environment.NewLine))
+                {
+                    rucksacks.Add(new Rucksack(items));
+                }
+            }
+
+            public int FindSumOfPriorities()
+            {
+                var sum = 0;
+                foreach (var r in rucksacks)
+                {
+                    sum += r.GetPriority();
+                }
+
+                return sum;
+            }
+
+            public class Rucksack
+            {
+                private HashSet<char> compartmentOne= new HashSet<char>();
+                private HashSet<char> compartmentTwo = new HashSet<char>();
+                private char error;
+
+                public Rucksack(string input) 
+                {
+                    // Sort into compartments
+                    for (int i = 0; i < input.Length; i++)
+                    {
+                        if (i < input.Length/2)
+                        {
+                            compartmentOne.Add(input[i]);
+                        }
+                        else
+                        {
+                            compartmentTwo.Add(input[i]);
+                        }
+                    }
+
+                    // Find error
+                    foreach (var item in compartmentTwo)
+                    {
+                        if (compartmentOne.TryGetValue(item, out _))
+                        {
+                            error = item;
+                        }
+                    }
+                }
+
+                public int GetPriority()
+                {
+                    // Capital letter
+                    if ((int)error < 97)
+                    {
+                        return (int)error - 64 + 26;
+                    }
+
+                    // Lower case letter
+                    return (int)error - 96;
+                }
+            }
+        }
+
         // == == == == == Puzzle 1 == == == == ==
         public static string Puzzle1(string input)
         {
-            return "Puzzle1";
+            var rr = new RucksackReorganization(input);
+            return rr.FindSumOfPriorities().ToString();
         }
 
         // == == == == == Puzzle 2 == == == == ==
--- AdventOfCodeTests/Day03Tests.cs
+++ AdventOfCodeTests/Day03Tests.cs
@@ -3,7 +3,6 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AdventOfCodeTests
 {
-    [Ignore]
     [TestClass]
     public class Day03Tests
     {
@@ -16,7 +15,7 @@ namespace AdventOfCodeTests
         {
             string day = "03";
             input_puzzle = Resources.Input.ResourceManager.GetObject($"D{day}_Puzzle").ToString();
-            input_example1 = string.Format("example{0}1", Environment.NewLine);
+            input_example1 = string.Format("vJrwpWtwJgWrhcsFMMfFFhFp{0}jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL{0}PmmdzqPrVvPwwTWBwg{0}wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn{0}ttgJtRGJQctTZtZT{0}CrZsJsPPZsGzwwsLwLmpwMDw", Environment.NewLine);
             input_example2 = string.Format("example{0}2", Environment.NewLine);
         }
 
@@ -33,7 +32,7 @@ namespace AdventOfCodeTests
             var result = AdventOfCode.Day03.Puzzle1(input_example1);
 
             // Assert
-            Assert.AreEqual($"Puzzle1", result);
+            Assert.AreEqual($"157", result);
         }
 
         [TestMethod]
@@ -43,7 +42,7 @@ namespace AdventOfCodeTests
             var result = AdventOfCode.Day03.Puzzle1(input_puzzle);
 
             // Assert
-            Assert.AreEqual($"Puzzle1", result);
+            Assert.AreEqual($"8105", result);
         }
 
         [TestMethod]
