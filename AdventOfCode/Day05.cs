e23e9b35cd61eba92d537d176fab862ba8f0d104
jlintvedt <jlintvedt@gmail.com>
2022-12-05 21:56:37 +0100

    Solve Day05 Puzzle1

--- AdventOfCode/Day05.cs
+++ AdventOfCode/Day05.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AdventOfCode
 {
@@ -7,10 +8,92 @@ namespace AdventOfCode
     /// </summary>
     public class Day05
     {
+        public class SupplyStacks
+        {
+            private List<Stack> stacks = new List<Stack>();
+            private readonly List<(int count, int from, int to)> instructions = new List<(int count, int from, int to)>();
+
+            public SupplyStacks(string input)
+            {
+                var segments = input.Split(new string[] { string.Format("{0}{0}", Environment.NewLine) }, StringSplitOptions.None);
+
+                // Parse stacks
+                var creates = segments[0].Split(Environment.NewLine);
+                var stackNumbers = creates[creates.Length-1].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                for (int i = 0; i < stackNumbers.Length; i++)
+                {
+                    stacks.Add(new Stack());
+                }
+
+                for (int i = creates.Length-2; i >= 0; i--)
+                {
+                    var row = creates[i];
+                    for (int j = 0; j < stacks.Count; j++)
+                    {
+                        stacks[j].AddCreate(row[1+j*4]);
+                    }
+                }
+
+                // Parse instructions
+                foreach (var row in segments[1].Split(Environment.NewLine))
+                {
+                    var inst = row.Split(" ");
+                    instructions.Add((int.Parse(inst[1]), int.Parse(inst[3]) - 1, int.Parse(inst[5]) - 1));
+                }
+            }
+
+            public string RearrangeAndFindSequence()
+            {
+                // Rearrange
+                foreach (var inst in instructions)
+                {
+                    stacks[inst.from].MoveToOtherStack(stacks[inst.to], inst.count);
+                }
+
+                // Find sequence
+                var sequence = "";
+                foreach (var stack in stacks)
+                {
+                    sequence += stack.TopCreate;
+                }
+
+                return sequence;
+            }
+
+            private class Stack
+            {
+                private Stack<char> creates = new Stack<char>();
+
+                public char TopCreate { get => creates.Peek(); }
+
+                public void AddCreate(char input)
+                {
+                    if (input != ' ')
+                        creates.Push(input);
+                }
+
+                public void MoveToOtherStack(Stack other, int count)
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        if (creates.TryPop(out char create))
+                        {
+                            other.AddCreate(create);
+                        }
+                        else
+                        {
+                            throw new InvalidOperationException();
+                        }
+                    }
+                }
+            }
+        }
+
         // == == == == == Puzzle 1 == == == == ==
         public static string Puzzle1(string input)
         {
-            return "Puzzle1";
+            var ss = new SupplyStacks(input);
+            return ss.RearrangeAndFindSequence();
         }
 
         // == == == == == Puzzle 2 == == == == ==
--- AdventOfCodeTests/Day05Tests.cs
+++ AdventOfCodeTests/Day05Tests.cs
@@ -3,7 +3,6 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AdventOfCodeTests
 {
-    [Ignore]
     [TestClass]
     public class Day05Tests
     {
@@ -16,7 +15,7 @@ namespace AdventOfCodeTests
         {
             string day = "05";
             input_puzzle = Resources.Input.ResourceManager.GetObject($"D{day}_Puzzle").ToString();
-            input_example1 = string.Format("example{0}1", Environment.NewLine);
+            input_example1 = string.Format("    [D]    {0}[N] [C]    {0}[Z] [M] [P]{0} 1   2   3 {0}{0}move 1 from 2 to 1{0}move 3 from 1 to 3{0}move 2 from 2 to 1{0}move 1 from 1 to 2", Environment.NewLine);
             input_example2 = string.Format("example{0}2", Environment.NewLine);
         }
 
@@ -33,7 +32,7 @@ namespace AdventOfCodeTests
             var result = AdventOfCode.Day05.Puzzle1(input_example1);
 
             // Assert
-            Assert.AreEqual($"Puzzle1", result);
+            Assert.AreEqual($"CMZ", result);
         }
 
         [TestMethod]
@@ -43,7 +42,7 @@ namespace AdventOfCodeTests
             var result = AdventOfCode.Day05.Puzzle1(input_puzzle);
 
             // Assert
-            Assert.AreEqual($"Puzzle1", result);
+            Assert.AreEqual($"TGWSMRBPN", result);
         }
 
         [TestMethod]
