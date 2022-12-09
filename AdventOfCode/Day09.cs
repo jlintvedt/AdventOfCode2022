ff87bc40b401cab746c8c0d454b379ed7a5f9530
jlintvedt <jlintvedt@gmail.com>
2022-12-09 19:19:37 +0100

    Solve Day09 Puzzle1

--- AdventOfCode/Day09.cs
+++ AdventOfCode/Day09.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AdventOfCode
 {
@@ -7,10 +8,103 @@ namespace AdventOfCode
     /// </summary>
     public class Day09
     {
+        public class Rope
+        {
+            private (int x, int y) head = (0, 0);
+            private (int x, int y) tail = (0, 0);
+            private readonly List<(char dir, int dist)> instructions = new List<(char dir, int dist)> ();
+            private HashSet<(int, int)> visited;
+
+            public Rope(string input)
+            {
+                foreach (var line in input.Split(Environment.NewLine))
+                {
+                    var parts = line.Split(' ');
+                    instructions.Add((parts[0][0], int.Parse(parts[1])));
+                }
+            }
+
+            public int FindNumSpacesVisitedByTail()
+            {
+                visited = new HashSet<(int, int)>{ tail };
+                foreach (var (dir, dist) in instructions)
+                {
+                    switch (dir)
+                    {
+                        case 'U':
+                            MoveUp(dist); break;
+                        case 'D':
+                            MoveDown(dist); break;
+                        case 'L':
+                            MoveLeft(dist); break;
+                        case 'R':
+                            MoveRight(dist); break;
+                        default:
+                            throw new ArgumentException();
+                    }
+                }
+
+                return visited.Count;
+            }
+
+            private void MoveUp(int dist)
+            {
+                for (int i = 0; i < dist; i++)
+                {
+                    head.y++;
+                    if (tail.y < head.y - 1)
+                    {
+                        tail = (head.x, head.y - 1);
+                        visited.Add(tail);
+                    }
+                }
+            }
+
+            private void MoveDown(int dist)
+            {
+                for (int i = 0; i < dist; i++)
+                {
+                    head.y--;
+                    if (tail.y > head.y + 1)
+                    {
+                        tail = (head.x, head.y + 1);
+                        visited.Add(tail);
+                    }
+                }
+            }
+
+            private void MoveLeft(int dist)
+            {
+                for (int i = 0; i < dist; i++)
+                {
+                    head.x--;
+                    if (tail.x > head.x + 1)
+                    {
+                        tail = (head.x + 1, head.y);
+                        visited.Add(tail);
+                    }
+                }
+            }
+
+            private void MoveRight(int dist)
+            {
+                for (int i = 0; i < dist; i++)
+                {
+                    head.x++;
+                    if (tail.x < head.x - 1)
+                    {
+                        tail = (head.x - 1, head.y);
+                        visited.Add(tail);
+                    }
+                }
+            }
+        }
+
         // == == == == == Puzzle 1 == == == == ==
         public static string Puzzle1(string input)
         {
-            return "Puzzle1";
+            var rope = new Rope(input);
+            return rope.FindNumSpacesVisitedByTail().ToString();
         }
 
         // == == == == == Puzzle 2 == == == == ==
--- AdventOfCodeTests/Day09Tests.cs
+++ AdventOfCodeTests/Day09Tests.cs
@@ -3,7 +3,6 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AdventOfCodeTests
 {
-    [Ignore]
     [TestClass]
     public class Day09Tests
     {
@@ -16,7 +15,7 @@ namespace AdventOfCodeTests
         {
             string day = "09";
             input_puzzle = Resources.Input.ResourceManager.GetObject($"D{day}_Puzzle").ToString();
-            input_example1 = string.Format("example{0}1", Environment.NewLine);
+            input_example1 = string.Format("R 4{0}U 4{0}L 3{0}D 1{0}R 4{0}D 1{0}L 5{0}R 2", Environment.NewLine);
             input_example2 = string.Format("example{0}2", Environment.NewLine);
         }
 
@@ -33,7 +32,7 @@ namespace AdventOfCodeTests
             var result = AdventOfCode.Day09.Puzzle1(input_example1);
 
             // Assert
-            Assert.AreEqual($"Puzzle1", result);
+            Assert.AreEqual($"13", result);
         }
 
         [TestMethod]
@@ -43,7 +42,7 @@ namespace AdventOfCodeTests
             var result = AdventOfCode.Day09.Puzzle1(input_puzzle);
 
             // Assert
-            Assert.AreEqual($"Puzzle1", result);
+            Assert.AreEqual($"6406", result);
         }
 
         [TestMethod]
