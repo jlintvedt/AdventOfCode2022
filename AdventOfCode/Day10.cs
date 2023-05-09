00b6a1a25bdf7214b1a1e157aca8e41324bf73f2
jlintvedt <jlintvedt@gmail.com>
2023-05-09 12:49:50 +0200

    Solve Day10 Puzzle1

--- AdventOfCode/Day10.cs
+++ AdventOfCode/Day10.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AdventOfCode
 {
@@ -7,10 +8,78 @@ namespace AdventOfCode
     /// </summary>
     public class Day10
     {
+        public class CRT
+        {
+            private int cycle = 1;
+            private int registry = 1;
+            private Queue<(Operation operation, int value)> instructions = new Queue<(Operation, int)>();
+
+            public CRT(string input) 
+            {
+                foreach (var line in input.Split(Environment.NewLine))
+                {
+                    var parts = line.Split(' ');
+                    if (parts.Length == 1)
+                    {
+                        instructions.Enqueue((Operation.noop, 0));
+                    } 
+                    else
+                    {
+                        instructions.Enqueue((Operation.addx, Int32.Parse(parts[1])));
+                    }
+                }
+            }
+
+            public int CalculateSignalStrength()
+            {
+                var x = 1;
+                var signalStrength = 0;
+                var inst = instructions.Dequeue();
+                var waited = 0;
+
+                for (int cycle = 1; ; cycle++)
+                {
+                    // Read signal strength at certain cycles
+                    if ((cycle - 20) % 40 == 0)
+                    {
+                        signalStrength += cycle * x;
+                    }
+
+                    // Addx operation
+                    if (inst.operation == Operation.addx)
+                    {
+                        if (++waited == 2)
+                        {
+                            x += inst.value;
+                        }
+                    }
+
+                    // If current instruction is done, check for next
+                    if (inst.operation == Operation.noop || waited == 2)
+                    {
+                        if (!instructions.TryDequeue(out inst))
+                        {
+                            break;
+                        }
+                        waited = 0;
+                    }
+                }
+
+                return signalStrength;
+            }
+
+            private enum Operation
+            {
+                noop,
+                addx
+            }
+        }
+
         // == == == == == Puzzle 1 == == == == ==
         public static string Puzzle1(string input)
         {
-            return "Puzzle1";
+            var crt = new CRT(input);
+            return crt.CalculateSignalStrength().ToString();
         }
 
         // == == == == == Puzzle 2 == == == == ==
--- AdventOfCodeTests/Day10Tests.cs
+++ AdventOfCodeTests/Day10Tests.cs
@@ -3,21 +3,18 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AdventOfCodeTests
 {
-    [Ignore]
     [TestClass]
     public class Day10Tests
     {
         private string input_puzzle;
         private string input_example1;
-        private string input_example2;
 
         [TestInitialize]
         public void LoadInput()
         {
             string day = "10";
             input_puzzle = Resources.Input.ResourceManager.GetObject($"D{day}_Puzzle").ToString();
-            input_example1 = string.Format("example{0}1", Environment.NewLine);
-            input_example2 = string.Format("example{0}2", Environment.NewLine);
+            input_example1 = string.Format("addx 15{0}addx -11{0}addx 6{0}addx -3{0}addx 5{0}addx -1{0}addx -8{0}addx 13{0}addx 4{0}noop{0}addx -1{0}addx 5{0}addx -1{0}addx 5{0}addx -1{0}addx 5{0}addx -1{0}addx 5{0}addx -1{0}addx -35{0}addx 1{0}addx 24{0}addx -19{0}addx 1{0}addx 16{0}addx -11{0}noop{0}noop{0}addx 21{0}addx -15{0}noop{0}noop{0}addx -3{0}addx 9{0}addx 1{0}addx -3{0}addx 8{0}addx 1{0}addx 5{0}noop{0}noop{0}noop{0}noop{0}noop{0}addx -36{0}noop{0}addx 1{0}addx 7{0}noop{0}noop{0}noop{0}addx 2{0}addx 6{0}noop{0}noop{0}noop{0}noop{0}noop{0}addx 1{0}noop{0}noop{0}addx 7{0}addx 1{0}noop{0}addx -13{0}addx 13{0}addx 7{0}noop{0}addx 1{0}addx -33{0}noop{0}noop{0}noop{0}addx 2{0}noop{0}noop{0}noop{0}addx 8{0}noop{0}addx -1{0}addx 2{0}addx 1{0}noop{0}addx 17{0}addx -9{0}addx 1{0}addx 1{0}addx -3{0}addx 11{0}noop{0}noop{0}addx 1{0}noop{0}addx 1{0}noop{0}noop{0}addx -13{0}addx -19{0}addx 1{0}addx 3{0}addx 26{0}addx -30{0}addx 12{0}addx -1{0}addx 3{0}addx 1{0}noop{0}noop{0}noop{0}addx -9{0}addx 18{0}addx 1{0}addx 2{0}noop{0}noop{0}addx 9{0}noop{0}noop{0}noop{0}addx -1{0}addx 2{0}addx -37{0}addx 1{0}addx 3{0}noop{0}addx 15{0}addx -21{0}addx 22{0}addx -6{0}addx 1{0}noop{0}addx 2{0}addx 1{0}noop{0}addx -10{0}noop{0}noop{0}addx 20{0}addx 1{0}addx 2{0}addx 2{0}addx -6{0}addx -11{0}noop{0}noop{0}noop", Environment.NewLine);
         }
 
         [TestMethod]
@@ -33,7 +30,7 @@ namespace AdventOfCodeTests
             var result = AdventOfCode.Day10.Puzzle1(input_example1);
 
             // Assert
-            Assert.AreEqual($"Puzzle1", result);
+            Assert.AreEqual($"13140", result);
         }
 
         [TestMethod]
@@ -43,14 +40,14 @@ namespace AdventOfCodeTests
             var result = AdventOfCode.Day10.Puzzle1(input_puzzle);
 
             // Assert
-            Assert.AreEqual($"Puzzle1", result);
+            Assert.AreEqual($"12840", result);
         }
 
         [TestMethod]
         public void Example_Puzzle2()
         {
             // Act
-            var result = AdventOfCode.Day10.Puzzle2(input_example2);
+            var result = AdventOfCode.Day10.Puzzle2(input_example1);
 
             // Assert
             Assert.AreEqual($"Puzzle2", result);
