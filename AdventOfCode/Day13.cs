31c7858091a7942f6926cc3ddacf5224b91f404d
jlintvedt <jlintvedt@gmail.com>
2023-05-12 08:50:50 +0200

    Solve Day13 Puzzle1

--- AdventOfCode/Day13.cs
+++ AdventOfCode/Day13.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AdventOfCode
 {
@@ -7,10 +8,169 @@ namespace AdventOfCode
     /// </summary>
     public class Day13
     {
+        public class DistressSignal
+        {
+            private List<Packet> packets = new List<Packet>();
+
+            public DistressSignal(string input) 
+            {
+                foreach (var pair in input.Split(string.Format("{0}{0}", Environment.NewLine)))
+                {
+                    var packageinputs = pair.Split(Environment.NewLine);
+                    packets.Add(new Packet(packageinputs[0]));
+                    packets.Add(new Packet(packageinputs[1]));
+                }
+            }
+
+            public int FindRightOrderIndicesSum()
+            {
+                var sum = 0;
+                for (int i = 0; i < packets.Count; i+=2)
+                {
+                    var p1 = packets[i];
+                    var p2 = packets[i+1];
+                    if (p1.ValidateOrder(p2))
+                        sum += i / 2 + 1;
+                }
+
+                return sum;
+            }
+
+            public class Packet
+            {
+                public Value value;
+
+                public Packet(string input)
+                {
+                    var inputPos = 0;
+                    value = new Value(input, ref inputPos);
+                }
+
+                public override string ToString()
+                {
+                    return $"Packet: {value}";
+                }
+
+                public bool ValidateOrder(Packet otherPacket)
+                {
+                    return value.ValidateOrder(otherPacket.value) ?? false;
+                }
+
+                public class Value
+                {
+                    public Vtype Type;
+                    public int IntegerValue;
+                    public List<Value> ListValues;
+
+                    public Value(string input, ref int inputPos)
+                    {
+                        // Recursive constructor!
+                        Type = input[inputPos] == '[' || input[inputPos] == ']' ? Vtype.list : Vtype.integer;
+
+                        if (Type == Vtype.integer)
+                        {
+                            var indexEnd = input.IndexOf(']', inputPos);
+                            var indexComma = input.IndexOf(',', inputPos);
+                            indexEnd = indexComma != -1 && indexComma < indexEnd ? indexComma : indexEnd;
+                            IntegerValue = int.Parse(input[inputPos..indexEnd]);
+                            inputPos = indexEnd - 1;
+                        }
+                        else
+                        {
+                            ListValues = new List<Value>();
+                            while (inputPos+1 < input.Length)
+                            {
+                                switch (input[++inputPos])
+                                {
+                                    case ']':
+                                        return;
+                                    case ',':
+                                        continue;
+                                    default:
+                                        ListValues.Add(new Value(input, ref inputPos));
+                                        break;
+                                }
+                            }
+                        }
+                    }
+
+                    public Value(int integerValue)
+                    {
+                        Type = Vtype.integer;
+                        IntegerValue = integerValue;
+                    }
+
+                    public bool? ValidateOrder(Value otherValue)
+                    {
+                        // Check types and convert to list as needed
+                        if (Type != otherValue.Type)
+                        {
+                            if (Type == Vtype.integer)
+                                ConvertToList();
+                            else
+                                otherValue.ConvertToList();
+                        }
+
+                        // Both Integers
+                        if (Type == Vtype.integer && otherValue.Type == Vtype.integer)
+                        {
+                            if (IntegerValue < otherValue.IntegerValue)
+                                return true;
+                            else if (IntegerValue > otherValue.IntegerValue)
+                                return false;
+                            else
+                                return null;
+                        }
+
+                        // Both Lists
+                        for (int i = 0; i < ListValues.Count; i++)
+                        {
+                            // Other list is empty -> order is not correct
+                            if (otherValue.ListValues.Count <= i)
+                                return false;
+
+                            var correctOrder = ListValues[i].ValidateOrder(otherValue.ListValues[i]);
+
+                            if (correctOrder == null)
+                                continue;
+
+                            return correctOrder;
+                        }
+
+                        // First list exhausted, order is correct if other list still has values
+                        if (ListValues.Count < otherValue.ListValues.Count)
+                            return true;
+
+                        // Still not possible to determine correct order
+                        return null;
+                    }
+
+                    public void ConvertToList()
+                    {
+                        Type = Vtype.list;
+                        ListValues = new List<Value> { new Value(IntegerValue) };
+                        IntegerValue = 0;
+                    }
+
+                    public override string ToString()
+                    {
+                        return Type == Vtype.integer ? $"{IntegerValue}" : $" [{string.Join(", ", ListValues)}] ";
+                    }
+                }
+
+                public enum Vtype
+                {
+                    integer,
+                    list
+                }
+            }
+        }
+
         // == == == == == Puzzle 1 == == == == ==
         public static string Puzzle1(string input)
         {
-            return "Puzzle1";
+            var ds = new DistressSignal(input);
+            return ds.FindRightOrderIndicesSum().ToString();
         }
 
         // == == == == == Puzzle 2 == == == == ==
--- AdventOfCodeTests/Day13Tests.cs
+++ AdventOfCodeTests/Day13Tests.cs
@@ -1,10 +1,10 @@
 using System;
 using AdventOfCodeTests.InputHelpers;
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AdventOfCodeTests
 {
-    [Ignore]
     [TestClass]
     public class Day13Tests
     {
@@ -16,7 +16,7 @@ namespace AdventOfCodeTests
         public void LoadInput()
         {
             input_puzzle = InputProvider.GetInput(2022, 13);
-            input_example1 = string.Format("example{0}1", Environment.NewLine);
+            input_example1 = string.Format("[1,1,3,1,1]{0}[1,1,5,1,1]{0}{0}[[1],[2,3,4]]{0}[[1],4]{0}{0}[9]{0}[[8,7,6]]{0}{0}[[4,4],4,4]{0}[[4,4],4,4,4]{0}{0}[7,7,7,7]{0}[7,7,7]{0}{0}[]{0}[3]{0}{0}[[[]]]{0}[[]]{0}{0}[1,[2,[3,[4,[5,6,7]]]],8,9]{0}[1,[2,[3,[4,[5,6,0]]]],8,9]", Environment.NewLine);
             input_example2 = string.Format("example{0}2", Environment.NewLine);
         }
 
@@ -33,7 +33,7 @@ namespace AdventOfCodeTests
             var result = AdventOfCode.Day13.Puzzle1(input_example1);
 
             // Assert
-            Assert.AreEqual($"Puzzle1", result);
+            Assert.AreEqual($"13", result);
         }
 
         [TestMethod]
@@ -43,7 +43,7 @@ namespace AdventOfCodeTests
             var result = AdventOfCode.Day13.Puzzle1(input_puzzle);
 
             // Assert
-            Assert.AreEqual($"Puzzle1", result);
+            Assert.AreEqual($"5852", result);
         }
 
         [TestMethod]
@@ -64,6 +64,102 @@ namespace AdventOfCodeTests
 
             // Assert
             Assert.AreEqual($"Puzzle2", result);
+        }
+
+        // Packet tests
+        [TestMethod]
+        public void PacketConstructor_SimpleList()
+        {
+            // Arrange
+            var input = "[1,2,3,4,5]";
+
+            // Act
+            var p = new AdventOfCode.Day13.DistressSignal.Packet(input);
+
+            // Assert
+            p.value.Type.Should().Be(AdventOfCode.Day13.DistressSignal.Packet.Vtype.list);
+            p.value.ListValues.Count.Should().Be(5);
+            p.value.ListValues[0].Type.Should().Be(AdventOfCode.Day13.DistressSignal.Packet.Vtype.integer);
+            p.value.ListValues[0].IntegerValue.Should().Be(1);
+            p.value.ListValues[4].Type.Should().Be(AdventOfCode.Day13.DistressSignal.Packet.Vtype.integer);
+            p.value.ListValues[4].IntegerValue.Should().Be(5);
+        }
+
+        // Packet tests
+        [TestMethod]
+        public void PacketConstructor_NestedList()
+        {
+            // Arrange
+            var input = "[[1],[2,3,4]]";
+
+            // Act
+            var p = new AdventOfCode.Day13.DistressSignal.Packet(input);
+
+            // Assert
+            p.value.Type.Should().Be(AdventOfCode.Day13.DistressSignal.Packet.Vtype.list);
+            p.value.ListValues.Count.Should().Be(2);
+
+            // Sub-list 1
+            p.value.ListValues[0].Type.Should().Be(AdventOfCode.Day13.DistressSignal.Packet.Vtype.list);
+            p.value.ListValues[0].ListValues.Count.Should().Be(1);
+
+            p.value.ListValues[0].ListValues[0].Type.Should().Be(AdventOfCode.Day13.DistressSignal.Packet.Vtype.integer);
+            p.value.ListValues[0].ListValues[0].IntegerValue.Should().Be(1);
+
+            // Sub-list 2
+            p.value.ListValues[1].Type.Should().Be(AdventOfCode.Day13.DistressSignal.Packet.Vtype.list);
+            p.value.ListValues[1].ListValues.Count.Should().Be(3);
+
+            p.value.ListValues[1].ListValues[0].Type.Should().Be(AdventOfCode.Day13.DistressSignal.Packet.Vtype.integer);
+            p.value.ListValues[1].ListValues[0].IntegerValue.Should().Be(2);
+            p.value.ListValues[1].ListValues[1].Type.Should().Be(AdventOfCode.Day13.DistressSignal.Packet.Vtype.integer);
+            p.value.ListValues[1].ListValues[1].IntegerValue.Should().Be(3);
+            p.value.ListValues[1].ListValues[2].Type.Should().Be(AdventOfCode.Day13.DistressSignal.Packet.Vtype.integer);
+            p.value.ListValues[1].ListValues[2].IntegerValue.Should().Be(4);
+        }
+
+        [TestMethod]
+        public void PacketConstructor_EmptyList()
+        {
+            // Arrange
+            var input = "[]";
+
+            // Act
+            var p = new AdventOfCode.Day13.DistressSignal.Packet(input);
+
+            // Assert
+            p.value.Type.Should().Be(AdventOfCode.Day13.DistressSignal.Packet.Vtype.list);
+            p.value.ListValues.Count.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void PacketConstructor_ValidateExampleInput()
+        {
+            // Arrange
+            var examples = input_example1.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            // Act & Assert
+            foreach (var input in examples)
+            {
+                var expected = input.Replace(",", ", ").Replace("[", " [").Replace("]", "] ");
+                var p = new AdventOfCode.Day13.DistressSignal.Packet(input);
+                p.value.ToString().Should().Be(expected);
+            }
+        }
+
+        [TestMethod]
+        public void PacketConstructor_ValidatePuzzleInput()
+        {
+            // Arrange
+            var examples = input_puzzle.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            // Act & Assert
+            foreach (var input in examples)
+            {
+                var expected = input.Replace(",", ", ").Replace("[", " [").Replace("]", "] ");
+                var p = new AdventOfCode.Day13.DistressSignal.Packet(input);
+                p.value.ToString().Should().Be(expected);
+            }
         }
     }
 }
