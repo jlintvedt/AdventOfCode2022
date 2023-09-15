0144540a9fe78fa558ae18bb2f66ce3076d3b51a
jlintvedt <jlintvedt@gmail.com>
2023-09-15 15:51:36 +0200

    Solve Day16 Puzzle1

--- AdventOfCode/Day16.cs
+++ AdventOfCode/Day16.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace AdventOfCode
 {
@@ -7,10 +10,120 @@ namespace AdventOfCode
     /// </summary>
     public class Day16
     {
+        public class ProboscideaVolcanium
+        {
+            private readonly Dictionary<string, Valve> valves = new Dictionary<string, Valve>();
+            private Valve currentValve;
+
+            public ProboscideaVolcanium(string input) 
+            {
+                var lines = input.Split(Environment.NewLine);
+                foreach (var line in lines)
+                {
+                    var name = line[6..8];
+                    valves.Add(name, new Valve(name, int.Parse(line[23..(line.IndexOf(';'))])));
+                }
+
+                foreach (var line in lines)
+                {
+                    var valve = valves[line[6..8]];
+                    var adjecent = line[(line.IndexOf(' ', 45) + 1)..].Split(", ");
+                    foreach (var a in adjecent)
+                        valve.adjacentValves.Add(valves[a]);
+                }
+
+                foreach (var valve in valves)
+                    valve.Value.CalculateDistances();
+
+                currentValve = valves["AA"];
+            }
+
+            public int FindMostPressureToRelease()
+            {
+                return RecFindPotentialPressure(currentValve, new Stack<string>(), 30);
+            }
+
+            public int RecFindPotentialPressure(Valve current, Stack<string> visited, int timeRemaining)
+            {
+                visited.Push(current.name);
+                var pressureRealeased = 0;
+                foreach (var valve in current.distances)
+                {
+                    if (visited.Contains(valve.Key))
+                        continue;
+
+                    var time = timeRemaining - valve.Value.dist - 1;
+                    if ((time) <= 0)
+                        continue;
+
+                    var pressure = time * valve.Value.flowRate;
+                    pressure += RecFindPotentialPressure(valves[valve.Key], visited, time);
+
+                    if (pressure > pressureRealeased)
+                        pressureRealeased = pressure;
+                }
+
+                visited.Pop();
+                return pressureRealeased;
+            }
+
+            public class Valve
+            {
+                public readonly string name;
+                public readonly int flowRate;
+                public bool open = false;
+                public List<Valve> adjacentValves = new List<Valve>();
+                public Dictionary<string, (int dist, int flowRate)> distances = new Dictionary<string, (int dist, int flowRate)>();
+
+                public Valve(string name, int flowRate)
+                {
+                    this.name = name;
+                    this.flowRate = flowRate;
+                }
+
+                public void CalculateDistances()
+                {
+                    RecFindShortestDistance(this, 0);
+
+                    // Remove zero-flow valves
+                    var zeroValves = new List<string>();
+                    foreach (var v in distances)
+                        if (v.Value.flowRate == 0)
+                            zeroValves.Add(v.Key);
+                    foreach (var v in zeroValves)
+                        distances.Remove(v);
+                }
+
+                private void RecFindShortestDistance(Valve current, int distance)
+                {
+                    // Check if visited previously by shorter path
+                    if (distances.TryGetValue(current.name, out var value))
+                        if (value.dist <= distance)
+                            return;
+                        else
+                            distances[current.name] = (distance, current.flowRate);
+                    else
+                        distances.Add(current.name, (distance, current.flowRate));
+
+                    // Remove 0-valves
+                    foreach (var a in current.adjacentValves)
+                        RecFindShortestDistance(a, distance + 1);
+                }
+
+                public override string ToString()
+                {
+                    var state = open ? "open" : "closed";
+                    return $"{name} {state}   FlowRate[{flowRate}]   Adjecent[{string.Join(",", adjacentValves.Select(v => v.name))}]";
+                }
+            }
+        }
+
         // == == == == == Puzzle 1 == == == == ==
         public static string Puzzle1(string input)
         {
-            return "Puzzle1";
+
+            var pv = new ProboscideaVolcanium(input);
+            return pv.FindMostPressureToRelease().ToString();
         }
 
         // == == == == == Puzzle 2 == == == == ==
--- AdventOfCodeTests/Day16Tests.cs
+++ AdventOfCodeTests/Day16Tests.cs
@@ -4,7 +4,6 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AdventOfCodeTests
 {
-    [Ignore]
     [TestClass]
     public class Day16Tests
     {
@@ -16,7 +15,7 @@ namespace AdventOfCodeTests
         public void LoadInput()
         {
             input_puzzle = InputProvider.GetInput(2022, 16);
-            input_example1 = string.Format("example{0}1", Environment.NewLine);
+            input_example1 = string.Format("Valve AA has flow rate=0; tunnels lead to valves DD, II, BB{0}Valve BB has flow rate=13; tunnels lead to valves CC, AA{0}Valve CC has flow rate=2; tunnels lead to valves DD, BB{0}Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE{0}Valve EE has flow rate=3; tunnels lead to valves FF, DD{0}Valve FF has flow rate=0; tunnels lead to valves EE, GG{0}Valve GG has flow rate=0; tunnels lead to valves FF, HH{0}Valve HH has flow rate=22; tunnel leads to valve GG{0}Valve II has flow rate=0; tunnels lead to valves AA, JJ{0}Valve JJ has flow rate=21; tunnel leads to valve II", Environment.NewLine);
             input_example2 = string.Format("example{0}2", Environment.NewLine);
         }
 
@@ -33,7 +32,7 @@ namespace AdventOfCodeTests
             var result = AdventOfCode.Day16.Puzzle1(input_example1);
 
             // Assert
-            Assert.AreEqual($"Puzzle1", result);
+            Assert.AreEqual($"1651", result);
         }
 
         [TestMethod]
@@ -43,7 +42,7 @@ namespace AdventOfCodeTests
             var result = AdventOfCode.Day16.Puzzle1(input_puzzle);
 
             // Assert
-            Assert.AreEqual($"Puzzle1", result);
+            Assert.AreEqual($"1751", result);
         }
 
         [TestMethod]
